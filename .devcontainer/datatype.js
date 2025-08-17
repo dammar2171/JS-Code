d6ebc42fe9a3043563f5461aa41828bdee9c01d9
Dammar Bhatt <169161652+dammar2171@users.noreply.github.com>
2025-08-17 03:06:47 +0000

    datatype

---
+++ .devcontainer/datatype.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version.
+
+let age=22; //number
+let name="dammar"; //string
+let isLoggedIn=false; //boolean 
+let num=115.56; //number 
+
+//null
+//object
+//undefined
+
+ 
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof num);
+
+
