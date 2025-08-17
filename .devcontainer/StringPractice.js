8d4e44590ad5442a6443a69f6e8d272ad6c81a1e
Dammar Bhatt <169161652+dammar2171@users.noreply.github.com>
2025-08-17 12:12:55 +0000

    string practice

---
+++ .devcontainer/StringPractice.js
@@ -0,0 +1,32 @@
+const greet="Namaste"
+const name="Dammar"
+const caste="bhatt"
+//console.log(`${greet} ${name} ${caste} how are you? `);
+
+//console.log(`${name.toUpperCase()}`);
+//console.log(name.toLowerCase());
+const fullname="dammar-bhatt"
+//console.log(fullname.split('-'));
+
+// console.log(fullname.charAt(6));
+// console.log(fullname.replace('d', 'B'));
+
+//console.log(fullname.substring(0,5));
+//console.log(fullname.includes('bhatt'));
+//console.log(fullname.indexOf('m'));
+
+const sentence="  hello my name is dammar bhatt and your?  "
+// console.log(sentence);
+// console.log(sentence.trim(" "));
+
+
+
+
+
+
+
+
+
+
+
+
