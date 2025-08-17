40604f8766e22f78ea72febc5f5c1117cb543a01
Dammar Bhatt <169161652+dammar2171@users.noreply.github.com>
2025-08-17 03:45:25 +0000

    conversion operation

---
+++ .devcontainer/conversionOperation.js
@@ -0,0 +1,26 @@
+let score=99;
+
+let scoreString=String(score);
+//console.log(scoreString);
+//console.log(typeof scoreString);
+
+ let str="99abc";
+
+ let strNum=Number(str);
+ //console.log(typeof strNum);
+ //console.log(strNum);
+
+ 
+ let isLoogedIn=true;
+
+ let isBooleanLoogedIn=Number(isLoogedIn);
+ console.log(isBooleanLoogedIn);
+ console.log(typeof isBooleanLoogedIn);
+ 
+ // "99"=>99 string to number conversion
+ //99=>"99" number to string conversion
+ //true=>1; and false=>0   boolean to number
+ //"99abc"=> NaN  
+ //"dammar"=>NaN string to number
+ 
+ 
