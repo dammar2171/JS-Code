be7939b1788c33293d3e43a98970c1f0fd9daf71
Dammar Bhatt <169161652+dammar2171@users.noreply.github.com>
2025-08-17 15:44:41 +0000

    numbers and math functions

---
+++ .devcontainer/numberMath.js
@@ -0,0 +1,33 @@
+const num = 100
+//console.log(num);
+
+//console.log(typeof num.toString());
+
+//console.log(num.toFixed(2));
+
+// console.log(num.toPrecision(3));
+// console.log(num.toPrecision(5));
+
+//console.log(typeof num.toLocaleString());
+
+
+
+/**********************MATH********************/
+
+//console.log(Math.random()*3);
+
+//console.log(Math.floor(Math.random()*3));
+
+const min=10
+const max=20
+//console.log(Math.floor(Math.random()*(max - min + 1) + min));
+//this is formula to generate  random number by giving range maximum and minimun number.
+
+// console.log(Math.max(1,2,3,4,5,6))
+//console.log(Math.min(1,2,34,5,6,78))
+
+
+
+
+
+
