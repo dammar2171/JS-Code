4c111bf9ae91fa37d68047b998ca1cc4bbcd4abd
Dammar Bhatt <169161652+dammar2171@users.noreply.github.com>
2025-08-18 12:08:00 +0000

    arrray

---
+++ .devcontainer/array.js
@@ -0,0 +1,40 @@
+let array1=[1,2,3,4,5]
+
+//console.log(array1);
+//console.log(array1[0]);
+array1.push(6)
+array1.push(7)
+array1.push(8)
+array1.push(9)
+array1.push(10)
+
+//array1.pop(10);
+//console.log(array1);
+
+let arr=[1,2,3];
+let brr=[4,5,6];
+
+let crr=arr.concat(brr);
+//console.log(crr);
+let drr=[...arr,...brr];
+//console.log(drr);
+
+//console.log(drr.length);
+
+//console.log(Array.isArray("dammar"));
+//console.log(Array.from("dammar"));
+//console.log(Array.from({name:"dammar"}));
+
+let score1=100;
+let score2=200;
+let score3=300;
+let score4=400;
+
+console.log(Array.of(score1,score2,score3,score4));
+
+
+
+
+
+
+
