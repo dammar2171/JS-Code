d9bda518776cabdc48301dcfd33e4d53723cfbe3
Dammar Bhatt <169161652+dammar2171@users.noreply.github.com>
2025-08-21 11:12:21 +0000

    control flow statement

---
+++ .devcontainer/controlFlow.js
@@ -0,0 +1,71 @@
+// if statement 
+
+if(true){
+  //  console.log("this statement is executed!");
+}
+
+// const a=13
+// if(a>0){
+//     console.log("this number is posative");
+// }
+// if(a<0){
+//     console.log("this number is negative");
+// }
+
+//program to check even or odd
+
+const num=15
+if(num%2==0){
+   // console.log(`${num } is a even number`);
+}
+if(num%2==1){
+    //console.log(`${num } is a odd number`);
+}
+
+//if-else statement
+
+if(true){
+    //code  ----True
+}else{
+    //code  ----False
+}
+
+const a=1
+if(a<10){
+    //console.log("a is less than 10");
+}else{
+    //console.log("a is greater than 10");
+}
+
+//Nested if statement
+
+if(true){
+    if(true){
+       // console.log("nested loop");
+    }
+}
+
+//if-else If-else statement
+
+//if(condition){
+    //code
+  //  }else if(condition){
+        //code
+ //   }else if(condition){
+        //code
+  //  }else{
+        //code
+  //  }
+
+//switch case statement
+
+const day=1
+switch(day){
+    case 1: //console.log("sunday");
+    break;
+    case 2:// console.log("monday");
+    break;
+    case 3: //console.log("tuesday");
+    break;
+    default: //console.log("not a day");
+}
