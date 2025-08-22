096701332976bec7d7997f50db40422b1ed69071
Dammar Bhatt <169161652+dammar2171@users.noreply.github.com>
2025-08-22 16:19:47 +0000

    filter methos

---
+++ .devcontainer/filter.js
@@ -0,0 +1,9 @@
+// filter is used to return a value by checking condition
+
+let number=[1,2,3,4,5,6,7,8]
+//const newNum=number.filter((num)=> num>3)
+
+const newNum=number.filter((num)=>( num>4))
+
+//const newNum=number.filter((num)=>{return num>4;})
+console.log(newNum);
